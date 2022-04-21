2156ff62a6977d830eb262b11a7d4f34f69d5c64
Bakiury <thecutaseta@gmail.com>
2022-04-21 11:47:23 -0500

    Swagger added to see our API better

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Social Network API')
+    .setDescription('All Rest API endpoints made with NestJs and Prisma will be displayed here')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
